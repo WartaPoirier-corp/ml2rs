1dc8af40f90ae021ea39a3f064ec12f9ec1e924a
Ana Gelez <ana@gelez.xyz>
2021-11-25 09:34:28 +0100

    Split code between bin and lib

--- src/ml2rs_lib/main.ml
+++ src/ml2rs_lib/main.ml
@@ -14,4 +14,17 @@
 (* If you delete or rename this file, you should add
    'src/ml2rs_lib/main.ml' to the 'skip' field in "drom.toml" *)
 
-let main () = Printf.printf "Hello world!\n"
+let read_file contents =
+    let lex = Lexing.from_string contents in
+    Parse.implementation lex
+
+let visit_parsetree tree =
+    let _ = List.map (fun (elt: Parsetree.structure_item) -> match elt.pstr_desc with
+        | Parsetree.Pstr_eval _ -> ()
+        | Parsetree.Pstr_value (_, bindings) -> List.fold_left (fun (_: unit) (binding: Parsetree.value_binding) -> match binding.pvb_pat.ppat_desc with
+            | Parsetree.Ppat_var name -> Printf.printf "found a let binding for %s\n" name.txt
+            | _ -> ()
+        ) () bindings
+        | _ -> failwith "oh no"
+    ) tree in
+    ()
