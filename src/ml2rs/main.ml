9ae4800b0b743817c2dc672e7df7dc36d6b6326d
Ana Gelez <ana@gelez.xyz>
2021-11-25 09:19:46 +0100

    Basic I/O + testing the parser

--- src/ml2rs/main.ml
+++ src/ml2rs/main.ml
@@ -11,4 +11,29 @@
 (**************************************************************************)
 
 
-let () = Ml2rs_lib.Main.main ()
+(* let () = Ml2rs_lib.Main.main () *)
+
+let help = "ml2rs -i <input> -o <output>"
+let input = ref ""
+let output = ref ""
+let args_spec = [
+    ("-i", Arg.Set_string input, "Input file (.ml)");
+    ("-o", Arg.Set_string output, "Output file (.rs)")
+]
+
+let () =
+    let () = Arg.parse args_spec (fun _ -> ()) help in
+    let input_file = open_in !input in
+    let contents = really_input_string input_file (in_channel_length input_file) in
+    let lex = Lexing.from_string contents in
+    let parse = Parse.implementation lex in
+    let _ = List.map (fun (elt: Parsetree.structure_item) -> match elt.pstr_desc with
+        | Parsetree.Pstr_eval _ -> ()
+        | Parsetree.Pstr_value (_, bindings) -> List.fold_left (fun (_: unit) (binding: Parsetree.value_binding) -> match binding.pvb_pat.ppat_desc with
+            | Parsetree.Ppat_var name -> Printf.printf "found a let binding for %s\n" name.txt
+            | _ -> ()
+        ) () bindings
+        | _ -> failwith "oh no"
+    ) parse in
+    Printf.printf "%s" contents
+    
